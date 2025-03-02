62d893d1c178e70eff54d10124a1aa08a841f843
zahoorahmadgorsi <zahoor.gorsi@gmail.com>
2025-03-03 01:49:11 +0500

    Initial Commit

---
+++ AppetiserCodeChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppetiserCodeChallenge
+//
+//  Created by Zahoor Gorsi on 03/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
